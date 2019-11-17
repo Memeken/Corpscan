08eca596ff6e28a487544fa2c2bc47107f2baac0
Memeken <coralie2319@hotmail.fr>
2019-11-17 22:35:41 +0100

    first commit

---
+++ menuUser.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<link rel="stylesheet" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">   
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Menu Admin</title>
+</head>
+<body>
+<br>
+
+
+		<div class="head">
+			<div class="LOGO">
+				<img class="logo" src="IMG_0168.jpg" alt="logoCorpScan" width="10%" >
+			</div>	
+			<div class='NAME'>
+				<br>
+				<br>
+				<h1>CORPSCAN</h1>
+			</div>
+			<div class="Menu">
+				<br>
+				<br>
+				<br>
+				<ul> <!-- Menu de navigation -->
+					<li><a href="?Acceuil">ACCUEIL</a></li>
+					<li><a href="?Test">TEST</a></li>
+					<li><a href="?Documentation">DOCUMENTATION</a></li>
+					<li><a href="?Stats">STATS</a></li>
+					<li><a href="?Contact">CONTACT</a></li>
+				</ul>
+			</div>
+			<br>
+			<br>
+			<br>
+			<div id="slider">
+				<figure>
+					<img src="asoggetti-C4HO6MzEWrU-unsplash.jpg" alt="img1" width="100%">
+					<img src="jerry-zhang-lLtBNeVOHFE-unsplash.jpg" alt="img2" width="100%">
+					<img src="john-towner-NfO6E5iQPAU-unsplash.jpg" alt="img3"width="100%">
+					<img src="kalen-emsley-_LuLiJc1cdo-unsplash.jpg" alt="img4" width="100%">
+					<img src="raquel-garcia-BAiSZDx4LgE-unsplash.jpg" alt="img5" width="100%">
+				</figure>
+			</div>
+		</div>
+
+	
+
+
+    
+</body>
+</html>
